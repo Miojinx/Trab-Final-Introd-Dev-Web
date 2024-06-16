eda885788602613051241e6aaf47dc060965cdba
Erik Heringer <erikther@gmail.com>
2024-06-16 20:03:49 -0300

    chore: dump do estoque inicializado

--- dump-estoque.sql
+++ dump-estoque.sql
@@ -18,7 +18,8 @@
 --
 -- Table structure for table `categorias`
 --
-
+create database estoque;
+use estoque;
 DROP TABLE IF EXISTS `categorias`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
