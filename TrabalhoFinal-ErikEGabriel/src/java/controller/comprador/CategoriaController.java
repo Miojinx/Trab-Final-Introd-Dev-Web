f52ce23d3a935204e36e5f6d9a6af464751aea99
Erik Heringer <erikther@gmail.com>
2024-06-21 18:41:59 -0300

    Chore: form, lista e controller da categoria iniciados

--- TrabalhoFinal-ErikEGabriel/src/java/controller/comprador/CategoriaController.java
+++ TrabalhoFinal-ErikEGabriel/src/java/controller/comprador/CategoriaController.java
@@ -1,13 +1,127 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controller.comprador;
 
-/**
- *
- * @author erikt
- */
-public class CategoriaController {
-    
+import Entidade.Categorias;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.servlet.RequestDispatcher;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.CategoriaDAO;
+
+@WebServlet(name = "CategoriaController", urlPatterns = {"/comprador/CategoriaController"})
+public class CategoriaController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // get parametro ação indicando o que fazer
+        String acao = (String) request.getParameter("acao");
+        Categorias categoria = new Categorias();
+        CategoriaDAO categoriaDAO = new CategoriaDAO();
+        RequestDispatcher rd;
+        switch (acao) {
+            case "Listar":
+                ArrayList<Categorias> listaCategorias = categoriaDAO.getAll();
+                request.setAttribute("listaCategorias", listaCategorias);
+
+                rd = request.getRequestDispatcher("/views/categoria/listaCategorias.jsp");
+                rd.forward(request, response);
+
+                break;
+            case "Alterar":
+            case "Excluir":
+
+                // get parametro ação indicando sobre qual categoria será a ação
+                int id = Integer.parseInt(request.getParameter("id"));
+                categoria = categoriaDAO.get(id);
+
+                request.setAttribute("categoria", categoria);
+                request.setAttribute("msgError", "");
+                request.setAttribute("acao", acao);
+
+                rd = request.getRequestDispatcher("/views/categoria/formCategorias.jsp");
+                rd.forward(request, response);
+                break;
+            case "Incluir":
+                request.setAttribute("categoria", categoria);
+                request.setAttribute("msgError", "");
+                request.setAttribute("acao", acao);
+
+                rd = request.getRequestDispatcher("/views/categoria/formCategorias.jsp");
+                rd.forward(request, response);
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        int id = Integer.parseInt(request.getParameter("id"));
+        String nomeCategoria = request.getParameter("nomeCategoria");
+        String btEnviar = request.getParameter("btEnviar");
+
+        RequestDispatcher rd;
+
+        if (nomeCategoria.isEmpty()) {
+            Categorias categoria = new Categorias();
+            switch (btEnviar) {
+                case "Alterar":
+                case "Excluir":
+                    try {
+                    CategoriaDAO categoriaDAO = new CategoriaDAO();
+                    categoria = categoriaDAO.get(id);
+
+                } catch (Exception ex) {
+                    System.out.println(ex.getMessage());
+                    throw new RuntimeException("Falha em uma query para cadastro de usuario");
+                }
+                break;
+            }
+
+            request.setAttribute("categoria", categoria);
+            request.setAttribute("acao", btEnviar);
+
+            request.setAttribute("msgError", "É necessário preencher todos os campos");
+
+            rd = request.getRequestDispatcher("/views/categoria/formCategorias.jsp");
+            rd.forward(request, response);
+
+        } else {
+            
+             Categorias categoria = new Categorias(id,nomeCategoria);
+             CategoriaDAO categoriaDAO = new CategoriaDAO();
+
+            try {
+                switch (btEnviar) {
+                    case "Incluir":
+                        categoriaDAO.insert(categoria);
+                        request.setAttribute("msgOperacaoRealizada", "Inclusão realizada com sucesso");
+                        break;
+                    case "Alterar":
+                        categoriaDAO.update(categoria);
+                        request.setAttribute("msgOperacaoRealizada", "Alteração realizada com sucesso");
+                        break;
+                    case "Excluir":
+                        categoriaDAO.delete(id);
+                        request.setAttribute("msgOperacaoRealizada", "Exclusão realizada com sucesso");
+                        break;
+                }
+
+                request.setAttribute("link", "/aplicacaoMVC/comprador/CategoriaController?acao=Listar");
+                rd = request.getRequestDispatcher("/views/comum/showMessage.jsp");
+                rd.forward(request, response);
+
+            } catch (IOException | ServletException ex) {
+                System.out.println(ex.getMessage());
+                throw new RuntimeException("Falha em uma query para cadastro de usuario");
+            }
+        }
+    }
+
 }
---
+++ TrabalhoFinal-ErikEGabriel/web/views/categoria/listaCategorias.jsp
@@ -0,0 +1,60 @@
+<%@page import="Entidade.Categorias"%>
+<%@page import="java.util.ArrayList"%>
+<%@page contentType="text/html" pageEncoding="UTF-8" %>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" href="#">
+        <title>Lista Categorias</title>
+         <link href="http://localhost:8080/aplicacaoMVC/views/bootstrap/bootstrap.min.css"  rel="stylesheet">
+
+    </head>
+    <body>
+        <div class="container">
+             <jsp:include page="../../comum/menu.jsp" />
+            <div class="mt-5">
+
+                <h2>Lista de Categorias</h2>
+
+                <a href="/aplicacaoMVC/comprador/CategoriaController?acao=Incluir" class="mb-2 btn btn-primary">Incluir</a>
+                <div class="table-responsive">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col">Id</th>
+                                <th scope="col">Nome da Categoria</th>
+                                <th scope="col">Ações</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <%
+                                ArrayList<Categorias> listaCategorias = (ArrayList<Categorias>) request.getAttribute("listaCategorias");
+
+                                for (Categorias categoria : listaCategorias) {
+                                    out.println("<tr>");
+                                    out.println("<th>" + categoria.getId() + "</th>");
+                                    out.println("<td>" + categoria.getNome_categoria()+ "</td>");
+                                    %>
+                            <td>
+                            <a href="/aplicacaoMVC/comprador/CategoriaController?acao=Alterar&id=<%=categoria.getId()%>" class="btn btn-warning">Alterar</a>
+                            <a href="/aplicacaoMVC/comprador/CategoriaController?acao=Excluir&id=<%=categoria.getId()%>" class="btn btn-danger">Excluir</a></td>
+                            
+                            <%   out.println("</tr>");
+                                }
+                            %>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+        <script src="http://localhost:8080/aplicacaoMVC/views/bootstrap/bootstrap.bundle.min.js"></script>
+
+    </body>
+</html>
+
